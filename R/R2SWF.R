3c81f96faec70676ae2e43ddff995f2da64da6a8
Yihui Xie <xie@yihui.name>
2011-01-23 20:52:12 -0600

    removed unnecessary arguments to simplify the usage; tweaking doc

--- R/R2SWF.R
+++ R/R2SWF.R
@@ -1,17 +1,16 @@
 ##' Convert PNG files to SWF.
-##' Given an R expression that can produce a sequence of images, this function can
-##' convert these images to a Flash file.
+##' Given an R expression that can produce a sequence of images, this
+##' function can record the images with the
+##' \code{\link[grDevices]{png}()} device and convert them to a Flash
+##' file.
 ##'
-##' @param expr an expression to generate the animation
+##' @param expr an expression to generate a sequence of images
 ##' @param interval the time interval between animation frames
 ##' @param output the name of the output
 ##' @param dev the graphics device to use (should be a PNG device but
-##' not limited to the \code{png()} in the \pkg{grDevice} package)
-##' @param filename the file name of the png images
-##' @param fmt the name format of the png images
-##' @param tmpdir the temporary directory to save all the intermediate
-##' png files
-##' @param para the (base) graphics parameters to set
+##' not limited to the \code{\link[grDevices]{png}()} device in the
+##' \pkg{grDevice} package)
+##' @param img.name the file name of the png images
 ##' @param ... other arguments to be passed to the graphics device
 ##' @return NULL
 ##' @author Yixuan Qiu \email{yixuan.qiu@@cos.name}
@@ -19,16 +18,14 @@
 ##'   for (i in 1:10) plot(runif(20), ylim = c(0, 1))
 ##' })
 png2swf <- function(expr, interval = 1, output = "movie.swf",
-    dev = "png", filename = "Rplot", fmt = "%03d",
-    tmpdir = tempdir(), para = list(),
-    ...) {
+    dev = "png", img.name = "Rplot", ...) {
     if (is.character(dev)) dev = get(dev)
-    dev(file.path(tmpdir, paste(filename, fmt, ".", "png", sep = "")), ...)
-    par(para)
+    dev(file.path(tempdir(), paste(filename, "%d.", "png", sep = "")), ...)
     eval(expr)
     dev.off()
 
-    png.files = list.files(tmpdir, "\\.png$", full.names = TRUE)
+    png.files = list.files(tempdir(), paste(img.name, "[0-9]*\\.png$", sep = ''),
+                           full.names = TRUE)
     .C("png2swf", png.files, length(png.files), as.numeric(interval),
         output, PACKAGE = "R2SWF")
     message("\nFlash has been created at: ", normalizePath(output))
