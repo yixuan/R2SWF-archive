34d541a24a087c61d84b7538745bedbbc47c6c9f
Yixuan Qiu <yixuan.qiu@cos.name>
2013-07-13 12:28:30 -0400

    set .onLoad() and .onUnload() functions

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
-.onLoad <- function(lib, pkg) {
-  library.dynam("R2SWF", pkg, lib)
-  .Call("swfInit", PACKAGE = "R2SWF");
+.onLoad <- function(libname, pkgname) {
+    library.dynam("R2SWF", pkgname, libname);
+    .Call("swfInit", PACKAGE = "R2SWF");
+    .add.default.fonts();
 }
+
+.onUnload <- function(libpath) {
+    .clean.fonts();
+    .C("Ming_collectGarbage", PACKAGE = "R2SWF");
+    library.dynam.unload("R2SWF", libpath);
+}
+
